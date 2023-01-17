b0e40c4c48b8272868656616425acb05a161e735
猫猫 <overflowcat@gmail.com>
2023-01-18 04:42:29 +0800

    fix: `top` in `callback()`

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class pdfLocation {
 
     scrollIntoView() {
         console.log("scrollIntoView");
-        window.lsPdfViewer.scrollPageIntoView({
+        top?.window.lsPdfViewer.scrollPageIntoView({
             pageNumber: this.page,
             destArray: [null, { name: "XYZ" },
                 this.left, this.top]
@@ -58,36 +58,20 @@ class pdfLocation {
     }
 }
 
-const left_arr = `
-<svg stroke="currentColor" fill="none" width="16" class="icon" stroke-width="2" stroke-linejoin="round" viewBox="0 0 24 24" stroke-linecap="round" height="16">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <line y1="12" x1="5" y2="12" x2="19"></line>
-    <line y1="16" x1="9" y2="12" x2="5"></line>
-    <line y1="8" x1="9" y2="12" x2="5"></line>
-</svg>`;
-
-const right_arr = `
-<svg stroke="currentColor" fill="none" width="16" class="icon" stroke-width="2" stroke-linejoin="round" viewBox="0 0 24 24" stroke-linecap="round" height="16">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <line y1="12" x1="5"  y2="12"  x2="19"></line>
-    <line y1="16" x1="15" y2="12" x2="19"></line>
-    <line y1="8"  x1="15" y2="12" x2="19"></line>
-</svg>`;
-
 function waitForElement(selector) {
     return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+        if (top?.document.querySelector(selector)) {
+            return resolve(top?.document.querySelector(selector));
         }
 
         const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
+            if (top?.document.querySelector(selector)) {
+                resolve(top?.document.querySelector(selector));
                 observer.disconnect();
             }
         });
 
-        observer.observe(document.querySelector("div#app-single-container"), {
+        observer.observe(top?.document.querySelector("div#app-single-container"), {
             childList: true,
             subtree: true
         });
@@ -98,10 +82,12 @@ function initialize(ele) {
     let pdfHistory = new PdfHistory();
 
     function callback(e) {
-        const ele = (window.e || e)?.srcElement;
+        isDebug && console.log({ top });
+        const ele = e.srcElement;
         if (ele.tagName !== 'A' || ele.className !== "internalLink")
             return;
-        console.log(window.lsPdfViewer.scroll);
+        const viewer = top?.lsPdfViewer;
+        console.log(viewer.scroll);
 
         /* An example of viewer._location
         {
@@ -112,22 +98,20 @@ function initialize(ele) {
             "rotation": 0,
             "pdfOpenParams": "#page=235&zoom=auto,0,784"
         } */
-        const { pageNumber, left, top } = window.lsPdfViewer._location;
-        const pre = new pdfLocation(pageNumber, left, top);
+        const pre = new pdfLocation(viewer._location.pageNumber, viewer._location.left, viewer._location.top);
         setTimeout(() => {
-            console.log(window.lsPdfViewer.scroll)
-            const { pageNumber, left, top } = window.lsPdfViewer._location;
-            const post = new pdfLocation(pageNumber, left, top);
+            console.log(viewer.scroll)
+            const post = new pdfLocation(viewer._location.pageNumber, viewer._location.left, viewer._location.top);
             pdfHistory.add(pre, post);
         }, 100);
         console.log({ pdfHistory });
     }
 
     setTimeout(() => {
-        const toolbar = document.querySelector(".extensions__pdf-toolbar > div > div.buttons");
-        let bck_btn = document.createElement("a");
+        const toolbar = top?.document.querySelector(".extensions__pdf-toolbar > div > div.buttons");
+        let bck_btn = top?.document.createElement("a");
         bck_btn.innerHTML = left_arr;
-        let fwd_btn = document.createElement("a");
+        let fwd_btn = top?.document.createElement("a");
         fwd_btn.innerHTML = right_arr;
         bck_btn.onclick = pdfHistory.back.bind(pdfHistory); // fix `this` issue
         fwd_btn.onclick = pdfHistory.forward.bind(pdfHistory);
@@ -138,18 +122,43 @@ function initialize(ele) {
         bck_btn.style.padding = "4px";
         toolbar.insertBefore(fwd_btn, toolbar.children[0]);
         toolbar.insertBefore(bck_btn, toolbar.children[0]);
-        document.addEventListener('click', callback, false);
+        top?.document.addEventListener('click', callback, false);
         toolbar.lastElementChild?.addEventListener('click', () => {
             pdfHistory = null;
-            document.removeEventListener('click', callback);
+            top?.document.removeEventListener('click', callback);
         });
     }, 1500);
 }
 
-while (true) {
-    if (document.querySelector("div.extensions__pdf-viewer") == null) {
-        const ele = await waitForElement("div.extensions__pdf-viewer");
-        initialize(ele);
+async function main() {
+    while (true) {
+        if (top?.document.querySelector("div.extensions__pdf-viewer") == null) {
+            const ele = await waitForElement("div.extensions__pdf-viewer");
+            initialize(ele);
+        }
+        await new Promise(r => setTimeout(r, 800));
     }
-    await new Promise(r => setTimeout(r, 1000));
 }
+
+const left_arr = `
+<svg stroke="currentColor" fill="none" width="16" class="icon" stroke-width="2" stroke-linejoin="round" viewBox="0 0 24 24" stroke-linecap="round" height="16">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <line y1="12" x1="5" y2="12" x2="19"></line>
+    <line y1="16" x1="9" y2="12" x2="5"></line>
+    <line y1="8" x1="9" y2="12" x2="5"></line>
+    </svg>`;
+
+const right_arr = `
+    <svg stroke="currentColor" fill="none" width="16" class="icon" stroke-width="2" stroke-linejoin="round" viewBox="0 0 24 24" stroke-linecap="round" height="16">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <line y1="12" x1="5"  y2="12"  x2="19"></line>
+    <line y1="16" x1="15" y2="12" x2="19"></line>
+    <line y1="8"  x1="15" y2="12" x2="19"></line>
+    </svg>`;
+
+const isDebug = true;
+
+logseq.ready(() => {
+    isDebug && logseq.App.showMsg("❤️ logseq-pdf-nav has bean loaded!");
+    main();
+}).catch(console.error);
